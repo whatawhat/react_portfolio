f7bd377d652cb139f57937bd272b8959b792b244
Stephanie <whatawhat@hotmail.com>
2021-08-17 18:44:24 -0400

    about me page

---
+++ src/components/aboutMe.js
@@ -0,0 +1,22 @@
+export default function AboutMe(){
+    return (
+        <div className="peronsalInfo" id="aboutMe">
+            <p>
+                I love puzzles. Some of my favorite puzzles include Sudoku, Killer Sudoku, and Samurai Sudoku. I enjoy jigsaw puzzles and even logic puzzles. My love for puzzles transfers to my career. I love challenges and enjoy problem solving and developing a solution or more if the problem calls for it.
+                
+                I teach students from a variety of levels who are blind or have a visual impairment. I collaborate with the educational team for each student. I hear problems from my fellow colleagues and have wanted to solve a new problem which led me to work towards becoming a web developer. I am very interested in web accessibility for everyone.
+                    
+                I apply my love for puzzles to help me grow and develop in the field of web development.
+                    
+                Feel free to reach out if you’d like to connect.
+            </p>
+            <div id="skillset">
+                <ol>
+                    <li className="skillList">Styling including CSS and Bootstrap</li>
+                    <li className="skillList">Javascript and NPM</li>
+                    <li className="skillList">GitHub and Heroku</li>
+                </ol>
+            </div>
+        </div>
+    )
+};
