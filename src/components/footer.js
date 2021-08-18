699cc0c218edf29879952e9e83a6c655c72b1f9b
Stephanie <whatawhat@hotmail.com>
2021-08-18 12:44:07 -0400

    added space between icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,23 +5,26 @@ import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 function Footer() {
     return (
       <footer className="w-100 mt-auto text-dark p-4">
-        <div>className="container text-center"</div>
-          <div id="email">
+        <div class="container text-center">
+          <div class="row m-5">
+          <div id="email" class="px-4">
             <a href="mailto:stephedavis12@gmail.com">
               <FaEnvelope size="2em"/>
             </a>
           </div>
 
-          <div id="github">
+          <div id="github" class="px-4">
             <a href="https://github.com/whatawhat">
               <FaGithub size="2em"/>
             </a>
           </div>
 
-          <div id="linkedIn">
+          <div id="linkedIn" class="px-4">
             <a href="www.linkedin.com/in/stephanie-davis-56780b3b">
               <FaLinkedin size="2em"/>
             </a>
+          </div>
+          </div>
           </div>
       </footer>
     );
