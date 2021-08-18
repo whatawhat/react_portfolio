5c86cac285958e823a67da1ed0a2ead62d11e47c
Stephanie <whatawhat@hotmail.com>
2021-08-18 07:36:54 -0400

    fixed links for accessibility

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,11 +6,17 @@ function Footer() {
     return (
       <footer className="w-100 mt-auto text-dark p-4">
         <div>className="container text-center"</div>
-            <a href="mailto:stephedavis12@gmail.com" target="_blank"/>
+            <a href="mailto:stephedavis12@gmail.com" target="_blank" rel="noreferrer">
+            <i className="fa fa-envelope" id='emailAnchor'></i> </a>
+
                 <FaEnvelope size="2em" color="ffffff"/>
-          <a href="https://github.com/whatawhat" target="_blank"/>
+          <a href="https://github.com/whatawhat" target="_blank" rel="noreferrer">
+          <i className="fa fa-github" id='githubAnchor'></i> </a>
+
             <FaGithub size="2em" color="#ffffff"/>
-          <a href="www.linkedin.com/in/stephanie-davis-56780b3b" target="_blank"/>
+          <a href="www.linkedin.com/in/stephanie-davis-56780b3b" target="_blank" rel="noreferrer">
+          <i className="fa fa-linkedin" id='linkedInAnchor'></i> </a>
+
             <FaLinkedin size="2em" color="#ffffff"/>
       </footer>
     );
