7a09c5b494488615b58a782d7bd20e72ebd2cd78
Stephanie <whatawhat@hotmail.com>
2021-08-18 11:42:33 -0400

    updated link to email and changed the color of the icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,24 @@
 import React from "react";
-//import {useLocation, useHistory} from 'react-router-dom';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
       <footer className="w-100 mt-auto text-dark p-4">
         <div>className="container text-center"</div>
-            <a href="mailto:stephedavis12@gmail.com" target="_blank" rel="noreferrer">
-            <i className="fa fa-envelope" id='emailAnchor'></i> </a>
-
-                <FaEnvelope size="2em" color="ffffff"/>
+          <div id="email">
+            <Link to="mailto:stephedavis12@gmail.com">
+              <FaEnvelope size="2em"/>
+            </Link>
+          </div>
           <a href="https://github.com/whatawhat" target="_blank" rel="noreferrer">
           <i className="fa fa-github" id='githubAnchor'></i> </a>
 
-            <FaGithub size="2em" color="#ffffff"/>
+            <FaGithub size="2em" color="#000000"/>
           <a href="www.linkedin.com/in/stephanie-davis-56780b3b" target="_blank" rel="noreferrer">
           <i className="fa fa-linkedin" id='linkedInAnchor'></i> </a>
 
-            <FaLinkedin size="2em" color="#ffffff"/>
+            <FaLinkedin size="2em" color="#000000"/>
       </footer>
     );
   };
