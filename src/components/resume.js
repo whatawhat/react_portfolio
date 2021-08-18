7c9b6fb2d5e91ce90a355776d5f4f53ac6e8be12
Stephanie <whatawhat@hotmail.com>
2021-08-18 19:35:32 -0400

    added resume to the class

--- src/components/resume.js
+++ src/components/resume.js
@@ -3,7 +3,7 @@ import React from "react";
 function Resume() {
     return (
         <a href="../assets/resume/SDavis Resume 08.25.20.pdf" download>
-            <button type="button" class="btn btn-lg">Click to view my Resume</button>
+            <button type="button" class="btn btn-lg resume">Click to view my Resume</button>
         </a>
     )
 }
