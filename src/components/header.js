94db63cd2a29139ff96cf3d20fbbf85ba5be1d19
Stephanie <whatawhat@hotmail.com>
2021-08-18 18:06:50 -0400

    changed font style

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,12 @@ import { NavLink} from "react-router-dom";
 
 function Header() {
         return (
-            <Nav className="nav nav-pills flex-column flex-lg-row px-4" style={{fontSize: 20}}>
+            <Nav className="nav nav-pills flex-column flex-lg-row px-4" class="nav" style={{fontSize: 20, fontStyle:"bold"}}>
                 <NavLink className="nav px-4" exact to="/">Home</NavLink>
-                <NavLink className="nav px-4" to="/projects">Projects</NavLink>
+                <NavLink className="nav px-4" to="/portfolio">Projects</NavLink>
                 <NavLink className="nav px-4" to="/contact">Contact Me</NavLink>
                 <NavLink className="nav px-4" to="/resume">Resume</NavLink>
             </Nav>
         );
     }
-
 export default Header;
